75ddbf90bebfa131fec1542cccff40e4707a2a0c
ikyrillos <ikyrillos@harborlab.com>
2022-04-13 14:24:24 +0300

    minor fix

--- src/main/java/com/writers/noteapp/controller/NoteController.java
+++ src/main/java/com/writers/noteapp/controller/NoteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class NoteController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity delete(Long id) {
+    public ResponseEntity delete(@PathVariable(name = "id") Long id) {
         log.info("Deleting note with id: {}", id);
         noteService.deleteById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
